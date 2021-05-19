fdf433cb4921036972049409f1e203ea293d9160
Govardhini Malla <68773317+mgovardhini9@users.noreply.github.com>
2021-05-19 23:03:11 +0530

    done

---
+++ Assets/Scripts/RobotBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotBehaviour : MonoBehaviour
+{
+    private Animator robotAnimator;
+    private int scoreAmount = 5;
+    private int lastScore = 0;
+    private int maxScore;
+    private float hitsToWin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        robotAnimator = this.GetComponent<Animator>();
+        maxScore = GameManager.gm.beatLevelScore;
+        hitsToWin = (float)(maxScore / scoreAmount);
+        StartCoroutine(RobotStartAnimationControl());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (GameManager.gm.score > lastScore)
+        {
+            lastScore = GameManager.gm.score;
+        }
+        
+    }
+
+    IEnumerator RobotStartAnimationControl()
+    {
+        int newScore = lastScore;
+        float animLength = robotAnimator.runtimeAnimatorController.animationClips[0].length;
+        float timeToWait = Mathf.Round( animLength / hitsToWin * 100f ) / 100f;
+        float timePassed = 0.0f;
+
+        while (animLength > timePassed + 0.3f)
+        {
+            if (newScore != lastScore)
+            {
+                robotAnimator.speed = 1f;
+                newScore = lastScore;
+                yield return new WaitForSeconds(timeToWait);
+                robotAnimator.speed = 0.0f;
+                timePassed += timeToWait;
+            }
+            yield return new WaitForEndOfFrame();
+        }
+
+        robotAnimator.speed = 1f;
+        robotAnimator.SetTrigger("Dance");
+    }
+}
---
+++ Assets/Scripts/RobotManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotManager : MonoBehaviour
+{
+    public GameObject robots;
+    private bool robotsInstantiated = false;
+    private int scoreAmount = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.gm.score >= scoreAmount && !robotsInstantiated)
+        {
+            robotsInstantiated = true;
+            GameObject spawnedRobots = Instantiate(robots, transform.position, transform.rotation) as GameObject;
+        }
+    }
+}
